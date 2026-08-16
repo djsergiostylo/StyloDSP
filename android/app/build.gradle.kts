c91bf5759898e85d3ce17fb8521c6ab55795712e
djsergiostylo <djsergiostylo@gmail.com>
2026-08-16 20:27:33 +0200

    android: align compile and target SDK to available stable API

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -4,12 +4,12 @@ plugins {
 
 android {
     namespace = "com.stylo.dsp"
-    compileSdk = 37
+    compileSdk = 36
 
     defaultConfig {
         applicationId = "com.stylo.dsp"
         minSdk = 26
-        targetSdk = 37
+        targetSdk = 36
         versionCode = 2
         versionName = "0.2.0"
 
