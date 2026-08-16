4c087c700f51c34950959682fc36235c5a910d77
djsergiostylo <djsergiostylo@gmail.com>
2026-08-16 19:57:19 +0200

    android: use stable CMake 3.22.1 toolchain

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -32,7 +32,7 @@ android {
     externalNativeBuild {
         cmake {
             path = file("src/main/cpp/CMakeLists.txt")
-            version = "4.1.2"
+            version = "3.22.1"
         }
     }
 }
